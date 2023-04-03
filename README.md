cf89dd220561ab871969415bb98215848401c705
Ian Baron <ian.baron@manchester.ac.uk>
2023-04-03 14:04:37 +0100

    initial commit - npm package to download mkcert binary to nodemodules/bin

---
+++ README.md
@@ -0,0 +1,5 @@
+# caddy-baron
+Downloads the mkcert binary for use in npm/npx scripts
+
+- Downloads the latest version of mkcert
+- No dependency on the unmaintained "download" or "request" packages
---
+++ index.mjs
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import got from 'got';
+import ndh from 'node-downloader-helper';
+
+let downloadDir = process.cwd().split('node-mkcert-binary')[0];
+downloadDir = `${downloadDir}.bin`;
+
+console.info(process.argv)
+
+switch (process.argv[2]) {
+  case "postinstall":
+    //get the latest release version
+    const latest = await got('https://api.github.com/repos/FiloSottile/mkcert/releases/latest').json();
+    const version = latest.tag_name;
+
+    // build mkcert download string
+    let platform = 'linux';
+    let arch = 'amd64';
+
+    switch(process.platform) {
+      case 'darwin':
+        platform = 'mac';
+      case 'windows':
+        platform = 'win32';
+    }
+
+    switch(process.arch) {
+      case 'arm64': arch = 'arm64';
+      case 'arm': arch = 'arm';
+    }
+
+    const fileString = `mkcert-${version}-${platform}-${arch}`;
+    if (platform === 'windows')
+      fileString = `${fileString}.exe`
+
+    const downloadString = `https://github.com/FiloSottile/mkcert/releases/download/${version}/${fileString}`;
+
+    fs.mkdir(downloadDir, { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+
+    const saveString = (platform === 'windows') ? 'mkcert.exe' : 'mkcert'
+
+    const dl = new ndh.DownloaderHelper(downloadString, downloadDir);
+    dl.on('error', (err) => console.log('Mkcert Download Failed', err));
+    dl.start().catch(err => console.error(err));
+    break;
+
+  case "preuninstall":
+    await fs.unlinkSync(`${downloadDir}${process.platform === 'win32' ? '.exe' : ''}`);
+    break;
+}
