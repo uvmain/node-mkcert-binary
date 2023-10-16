0438814c073c3dda405af2df2774eb6a6b41190c
Ian Baron <ian.baron@manchester.ac.uk>
2023-10-16 09:37:38 +0100

    use native http instead of got package

--- index.mjs
+++ index.mjs
@@ -1,57 +1,64 @@
 import fs, { createWriteStream } from 'fs';
-import got, { create } from 'got';
+import https from 'https';
+import { URL } from 'url';
+import path from 'path';
 
 let downloadDir = process.cwd().split('node-mkcert-binary')[0];
 downloadDir = `${downloadDir}.bin`;
 
-console.info(process.argv)
-
 switch (process.argv[2]) {
   case "postinstall":
     //get the latest release version
-    const latest = await got('https://api.github.com/repos/FiloSottile/mkcert/releases/latest').json();
-    const version = latest.tag_name;
+    const latestUrl = new URL('https://api.github.com/repos/FiloSottile/mkcert/releases/latest');
+    latestUrl.headers = {'User-Agent': 'Node.js'};
+    https.get(latestUrl, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', async () => {
+        const latest = JSON.parse(data);
+        const version = latest.tag_name;
 
-    // build mkcert download string
-    let platform = process.platform;
-    let saveString = 'mkcert';
+        // build mkcert download string
+        let platform = process.platform;
+        let saveString = 'mkcert';
 
-    const arch = (process.arch == 'x64') ? 'amd64' : process.arch
+        const arch = (process.arch == 'x64') ? 'amd64' : process.arch
 
-    let fileString = `mkcert-${version}-${platform}-${arch}`;
+        let fileString = `mkcert-${version}-${platform}-${arch}`;
 
-    if (platform === 'win32') {
-      fileString = `mkcert-${version}-windows-${arch}.exe`;
-      saveString = 'mkcert.exe';
-    }
+        if (platform === 'win32') {
+          fileString = `mkcert-${version}-windows-${arch}.exe`;
+          saveString = 'mkcert.exe';
+        }
 
-    console.log(fileString);
+        const downloadUrl = new URL(`https://github.com/FiloSottile/mkcert/releases/download/${version}/${fileString}`);
 
-    const downloadString = `https://github.com/FiloSottile/mkcert/releases/download/${version}/${fileString}`;
+        fs.mkdir(downloadDir, { recursive: true }, (err) => {
+          if (err) throw err;
+        });
 
-    console.log(downloadString);
+        const fileStream = createWriteStream(path.join(downloadDir, saveString));
 
-    fs.mkdir(downloadDir, { recursive: true }, (err) => {
-      if (err) throw err;
+        https.get(downloadUrl, (res) => {
+          res.pipe(fileStream);
+          fileStream.on('finish', () => {
+            fileStream.close();
+          });
+        }).on('error', (error) => {
+          console.log(`Failed to download mkcert: ${error}`);
+          fs.unlinkSync(path.join(downloadDir, saveString));
+        });
+
+      });
+    }).on('error', (error) => {
+      console.log(`Failed to get latest release: ${error}`);
     });
-
-    const downloadStream = got.stream(downloadString)
-    const fileStream = createWriteStream(`${downloadDir}/${saveString}`)
-
-    downloadStream
-      .on('error', (error) => {
-        console.log(`Failed to download mkcert: ${error}`)
-      })
-    fileStream
-    .on('error', (error) => {
-      console.log(`Failed to download mkcert: ${error}`)
-    })
-
-    downloadStream.pipe(fileStream)
 
     break;
 
   case "preuninstall":
-    await fs.unlinkSync(`${downloadDir}${process.platform === 'win32' ? '.exe' : ''}`);
+    await fs.unlinkSync(path.join(downloadDir, `${process.platform === 'win32' ? 'mkcert.exe' : 'mkcert'}`));
     break;
 }
