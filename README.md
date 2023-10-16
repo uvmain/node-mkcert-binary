ed823d6fc9c6a9eaf7a13f2d400f05fbe1a8149f
Ian Baron <ian.baron@manchester.ac.uk>
2023-10-16 11:16:36 +0100

    remove all dependencies, use 'bin' in package.json to correctly copy to node_modules/.bin

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Downloads the mkcert binary for use in npm/npx scripts
 
 - Downloads the latest version of mkcert
-- No dependency on the unmaintained "download" or "request" packages
+- No dependencies
 
 # Example npm script usage
 ```
--- index.mjs
+++ index.mjs
@@ -1,64 +1,69 @@
-import fs, { createWriteStream } from 'fs';
+import fs from 'fs';
 import https from 'https';
 import { URL } from 'url';
-import path from 'path';
 
-let downloadDir = process.cwd().split('node-mkcert-binary')[0];
-downloadDir = `${downloadDir}.bin`;
+let saveString = 'mkcert';
 
-switch (process.argv[2]) {
-  case "postinstall":
-    //get the latest release version
-    const latestUrl = new URL('https://api.github.com/repos/FiloSottile/mkcert/releases/latest');
-    latestUrl.headers = {'User-Agent': 'Node.js'};
+async function getLatestReleaseTag() {
+  const latestUrl = new URL('https://api.github.com/repos/FiloSottile/mkcert/releases/latest');
+  latestUrl.headers = { 'User-Agent': 'Node.js' };
+
+  return new Promise((resolve, reject) => {
     https.get(latestUrl, (res) => {
-      let data = '';
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-      res.on('end', async () => {
-        const latest = JSON.parse(data);
-        const version = latest.tag_name;
-
-        // build mkcert download string
-        let platform = process.platform;
-        let saveString = 'mkcert';
-
-        const arch = (process.arch == 'x64') ? 'amd64' : process.arch
-
-        let fileString = `mkcert-${version}-${platform}-${arch}`;
-
-        if (platform === 'win32') {
-          fileString = `mkcert-${version}-windows-${arch}.exe`;
-          saveString = 'mkcert.exe';
-        }
-
-        const downloadUrl = new URL(`https://github.com/FiloSottile/mkcert/releases/download/${version}/${fileString}`);
-
-        fs.mkdir(downloadDir, { recursive: true }, (err) => {
-          if (err) throw err;
+      if (res.statusCode === 302 && res.headers.location) {
+        // If a redirect is encountered, resolve with the new URL
+        resolve(new URL(res.headers.location));
+      } else {
+        let rawData = '';
+        res.on('data', (chunk) => {
+          rawData += chunk;
         });
-
-        const fileStream = createWriteStream(path.join(downloadDir, saveString));
-
-        https.get(downloadUrl, (res) => {
-          res.pipe(fileStream);
-          fileStream.on('finish', () => {
-            fileStream.close();
-          });
-        }).on('error', (error) => {
-          console.log(`Failed to download mkcert: ${error}`);
-          fs.unlinkSync(path.join(downloadDir, saveString));
+        res.on('end', () => {
+          const parsedData = JSON.parse(rawData);
+          resolve(parsedData.tag_name);
         });
-
-      });
-    }).on('error', (error) => {
-      console.log(`Failed to get latest release: ${error}`);
+      }
     });
-
-    break;
-
-  case "preuninstall":
-    await fs.unlinkSync(path.join(downloadDir, `${process.platform === 'win32' ? 'mkcert.exe' : 'mkcert'}`));
-    break;
+  });
 }
+
+async function getLatestReleaseUrl() {
+  const version = await getLatestReleaseTag();
+  const platform = process.platform;
+  const arch = process.arch === 'x64' ? 'amd64' : process.arch;
+  let fileString = `mkcert-${version}-${platform}-${arch}`;
+
+  if (platform === 'win32') {
+    fileString = `mkcert-${version}-windows-${arch}.exe`;
+    saveString = 'mkcert.exe';
+  }
+
+  const latestUrl = `https://github.com/FiloSottile/mkcert/releases/download/${version}/${fileString}`;
+  console.log(latestUrl);
+  return new URL(latestUrl);
+}
+
+async function downloadFile(downloadUrl) {
+  downloadUrl.headers = { 'User-Agent': 'Node.js' };
+  https.get(downloadUrl, (res) => {
+    if (res.statusCode === 302 && res.headers.location) {
+      // If a redirect is encountered, follow the new location
+      downloadFile(new URL(res.headers.location));
+    } else if (res.statusCode === 200) {
+      const writeStream = fs.createWriteStream(saveString);
+
+      res.pipe(writeStream);
+
+      writeStream.on('finish', () => {
+        writeStream.close();
+        console.log('Download Completed');
+      });
+    } else {
+      console.error(`Failed to download. HTTP status code: ${res.statusCode}`);
+    }
+  });
+}
+
+const downloadUrl = await getLatestReleaseUrl();
+
+await downloadFile(downloadUrl);
