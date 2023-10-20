3a7315e9c13de93e364a0782396a03e32b2ab687
Ian Baron <ian.baron@manchester.ac.uk>
2023-10-20 11:55:15 +0100

    fix binary js for linux

--- bin/mkcert.js
+++ bin/mkcert.js
@@ -3,6 +3,6 @@
 const { spawn } = require('child_process');
 
 const args = process.argv.slice(2);
-const binaryName = (process.platform === 'win32') ? 'mkcert.exe' : 'mkcert';
+const binaryName = (process.platform === 'win32') ? 'mkcert.exe' : './mkcert';
 
 const child = spawn(binaryName, args, { cwd: __dirname });
