5d665bcb893a67ca4a7b47ec050823f587141827
Ian Baron <ian.baron@manchester.ac.uk>
2023-10-20 09:11:18 +0100

    it works on Windows, tested, for reals this time

--- index.mjs
+++ index.mjs
@@ -2,47 +2,19 @@ import fs from 'fs';
 import https from 'https';
 import { URL } from 'url';
 
-const platform = process.platform;
+const platform = (process.platform === 'win32') ? 'windows' : 'linux';
+const saveString = (process.platform === 'win32') ? 'mkcert.exe' : 'mkcert';
 const arch = process.arch === 'x64' ? 'amd64' : process.arch;
-const saveString = (platform === 'win32') ? 'mkcert.exe' : 'mkcert';
-
 const options = { headers: { 'User-Agent': 'Node.js' } };
 
-async function getLatestReleaseTag() {
-  const latestUrl = new URL('https://api.github.com/repos/FiloSottile/mkcert/releases/latest');
-
-  return new Promise((resolve, reject) => {
-    const request = https.get(latestUrl, options, (res) => {
-      if (res.statusCode === 302 && res.headers.location) {
-        resolve(new URL(res.headers.location));
-      } else {
-        let rawData = '';
-        res.on('data', (chunk) => {
-          rawData += chunk;
-        });
-        res.on('end', () => {
-          const parsedData = JSON.parse(rawData);
-          resolve(parsedData.tag_name);
-        });
-      }
-    });
-
-    request.on('error', (error) => {
-      reject(error);
-    });
-  });
-}
-
 async function getLatestReleaseUrl() {
-  const version = await getLatestReleaseTag();
-  const fileString = (platform === 'win32') ? `mkcert-${version}-windows-${arch}.exe` : `mkcert-${version}-${platform}-${arch}`;
-  const latestUrl = `https://github.com/FiloSottile/mkcert/releases/download/${version}/${fileString}`;
+  const latestUrl = `https://dl.filippo.io/mkcert/latest?for=${platform}/${arch}`
   return new URL(latestUrl);
 }
 
 async function downloadFile(downloadUrl, saveString) {
   const request = https.get(downloadUrl, options, (res) => {
-    if (res.statusCode === 302 && res.headers.location) {
+    if ([301, 302].includes(res.statusCode) && res.headers.location) {
       downloadFile(new URL(res.headers.location), saveString);
     } else if (res.statusCode === 200) {
       const writeStream = fs.createWriteStream(saveString);
